16327cc978ade0f917a3d362d1e775260609c15c
Elio Cortes <elio.cortes3000@gmail.com>
2022-11-08 21:19:23 -0500

    resize font

---
+++ src/components/ResizeFont.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResizeFont = () => {
+  return (
+    <div className="change-font">
+      Hello World! 
+      Hello World!
+    </div>
+  )
+}
+
+export default ResizeFont;
